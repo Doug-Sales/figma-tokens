a73d79830ea709e2f717e94015fadb420954438a
Doug-Sales <douglasmsales28@outlook.com>
2023-04-12 13:59:11 -0300

    Initial commit

---
+++ build/js/token.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 12 Apr 2023 16:33:09 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorBlue": "#1e3a8a",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#6b6b6b",
+  "ColorGray800": "#262626",
+  "ColorGray900": "#121214"
+};
